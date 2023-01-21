36add069c08565cf766c7dce58306a24be54408a
Atilla Süer <suer.atilla@gmail.com>
2023-01-21 12:31:34 +0300

    Proje dosyası ekle.

---
+++ 03.AriBilgi.Blog.Data/Migrations/20221105073257_pcguncelleme2.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace _03.AriBilgi.Blog.Data.Migrations
+{
+    public partial class pcguncelleme2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
